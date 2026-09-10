d919c7ce0b9b1bcf276fb8f88712152fabe8e396
elas-independencia <mmarcielbarbosadelima@gmail.com>
2026-09-10 02:02:25 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function mostrarMensagem(area) {
+
+    const mensagens = {
+
+        "Finanças":
+            "Aqui você encontrará informações sobre organização e planejamento financeiro.",
+
+        "Documentação":
+            "Aqui você encontrará informações sobre documentos e serviços.",
+
+        "Moradia":
+            "Aqui você encontrará informações introdutórias sobre aluguel, compra e planejamento de moradia.",
+
+        "Carreira":
+            "Aqui você encontrará informações sobre capacitação profissional e oportunidades.",
+
+        "Planejamento":
+            "Aqui você encontrará informações para ajudar no planejamento de decisões financeiras.",
+
+        "Tecnologia":
+            "Aqui você encontrará informações sobre tecnologia e acesso à informação."
+
+    };
+
+    alert(mensagens[area]);
+
+}
